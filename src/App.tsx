ece744716125bccc09d0310fd314da355d8dbd12
Jon Smith <smithj@servercore.io>
2022-07-04 11:04:46 +1000

    Incompatible types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { selectAirQualityStations } from "./services/api";
+import { useAppSelector } from "./store";
 import "./styles.css";
 
 export default function App() {
+  const airQualityStations = useAppSelector(selectAirQualityStations);
+
+  // $ tsc --pretty --noEmit
+  // src/App.tsx:6:45 - error TS2345: Argument of type '((state: { api: CombinedState<ReplaceTagTypes<{}, "airQualityStations"> & { getStations: QueryDefinition<void, BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, "airQualityStations", AirQualityStation[], "api">; }, "airQualityStations", "api">; }) => AirQualityStation[] | undefin...' is not assignable to parameter of type '(state: { api: CombinedState<{}, never, "api">; }) => AirQualityStation[] | undefined'.
+  //   Types of parameters 'state' and 'state' are incompatible.
+  //     Type '{ api: CombinedState<{}, never, "api">; }' is not assignable to type '{ api: CombinedState<ReplaceTagTypes<{}, "airQualityStations"> & { getStations: QueryDefinition<void, BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>, "airQualityStations", AirQualityStation[], "api">; }, "airQualityStations", "api">; }'.
+  //       The types of 'api.provided' are incompatible between these types.
+  //         Property 'airQualityStations' is missing in type 'InvalidationState<never>' but required in type 'InvalidationState<"airQualityStations">'.
+
   return (
     <div className="App">
         RTK Typing of Selector
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createSelector } from "@reduxjs/toolkit";
+
+export interface AirQualityStation {
+    station_id: string;
+    station_name: string;
+    region_id: string
+    link: string;
+    start_date: string;
+    end_date: string;
+    latitude: number;
+    longitude: number;
+}
 
 export const api = createApi({
     reducerPath: "api",
@@ -8,3 +20,26 @@ export const api = createApi({
     endpoints: () => ({})
 });
 
+export const airQualityApi = api.enhanceEndpoints({
+    addTagTypes: ["airQualityStations"]
+}).injectEndpoints({
+    overrideExisting: true,
+    endpoints: (builder) => ({
+        getStations: builder.query<AirQualityStation[], void>({
+            query: () => ({
+                url: "/stations?pagesize=10&pagenumber=1",
+                method: "GET",
+            }),
+            providesTags: ["airQualityStations"]
+        })
+    })
+})
+
+export const { useGetStationsQuery } = airQualityApi;
+
+export const selectAirQualityStationResults = airQualityApi.endpoints.getStations.select();
+
+export const selectAirQualityStations = createSelector(
+    selectAirQualityStationResults,
+    (results) => results.data
+);
