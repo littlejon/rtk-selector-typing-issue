f4124813613173e6b94f1a009dcf81decfa4da53
Jon Smith <smithj@servercore.io>
2022-07-04 11:06:51 +1000

    Types working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { selectAirQualityStations } from "./services/api";
+import { useAppSelector } from "./store";
 import "./styles.css";
 
 export default function App() {
+  const airQualityStations = useAppSelector(selectAirQualityStations);
+
   return (
     <div className="App">
         RTK Typing of Selector
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,39 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createSelector } from "@reduxjs/toolkit";
+
+export interface AirQualityStation {
+    station_id: string;
+    station_name: string;
+    region_id: string
+    link: string;
+    start_date: string;
+    end_date: string;
+    latitude: number;
+    longitude: number;
+}
 
 export const api = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-      baseUrl: "https://airquality.des.qld.gov.au/v1",
-    }),
-    endpoints: () => ({})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://airquality.des.qld.gov.au/v1",
+  }),
+  tagTypes: ["airQualityStations"],
+  endpoints: (builder) => ({
+      getStations: builder.query<AirQualityStation[], void>({
+          query: () => ({
+              url: "/stations?pagesize=10&pagenumber=1",
+              method: "GET",
+          }),
+          providesTags: ["airQualityStations"]
+      })
+  })
 });
 
+export const { useGetStationsQuery } = api;
+
+export const selectAirQualityStationResults = api.endpoints.getStations.select();
+
+export const selectAirQualityStations = createSelector(
+  selectAirQualityStationResults,
+  (results) => results.data
+);
